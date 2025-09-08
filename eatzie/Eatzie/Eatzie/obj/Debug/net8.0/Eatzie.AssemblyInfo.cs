aac98b30015d4aeab5f90c760157442fe98c1d84
jingji <hieu0901179787@gmail.com>
2025-09-08 13:13:06 +0700

    ssss

--- eatzie/Eatzie/Eatzie/obj/Debug/net8.0/Eatzie.AssemblyInfo.cs
+++ eatzie/Eatzie/Eatzie/obj/Debug/net8.0/Eatzie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eatzie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240fd2da0c0d14f37a9fd88e50048f11301a67a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a2c5ee014e2c32787afb76beaa78c089251ee93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eatzie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eatzie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
